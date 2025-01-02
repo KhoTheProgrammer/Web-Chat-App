7c8655b9770b7e8279f15bb7d41768757343e760
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-02 23:21:22 +0200

    implemented a sidebar component

--- apps/client/src/Components/Sidebar.jsx
+++ apps/client/src/Components/Sidebar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { CiHome } from "react-icons/ci";
+import { FaRegMessage } from "react-icons/fa6";
+import { CiSearch } from "react-icons/ci";
+import { CiSettings } from "react-icons/ci";
 
 const Sidebar = () => {
   return (
-    <div>
-      
+    <div className="h-screen flex flex-col justify-between w-[88px] shadow-[10px_0_15px_-5px_rgba(0,0,0,0.1)] px-8 pt-4">
+      {/* Top icons */}
+      <div className="flex flex-col space-y-4 ">
+        <CiHome size={24} color="black" />
+        <FaRegMessage size={24} color="#615ef0" />
+        <CiSearch size={24} color="black" />
+      </div>
+      {/* Bottom icon */}
+      <div className="flex items-center justify-center">
+        <CiSettings size={24} color="black" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
