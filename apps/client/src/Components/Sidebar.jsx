8e79a0e1ae13b292555ca444702bd3ab06991def
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-03 00:39:11 +0200

    Set minimum height to screen of the device and reduced the width

--- apps/client/src/Components/Sidebar.jsx
+++ apps/client/src/Components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { CiSettings } from "react-icons/ci";
 
 const Sidebar = () => {
   return (
-    <div className="h-screen flex flex-col justify-between w-[10%] shadow-[10px_0_15px_-5px_rgba(0,0,0,0.1)] items-center pt-4">
+    <div className="min-h-screen flex flex-col justify-between w-[5%] shadow-[10px_0_15px_-5px_rgba(0,0,0,0.1)] items-center pt-4">
       {/* Top icons */}
       <div className="flex flex-col space-y-4 ">
         <CiHome size={24} color="black" />
