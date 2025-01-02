7ce85126c6c26184cee2a58c464d5359492c58fb
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-03 01:31:38 +0200

    Imported and rendered components used to build web chat app inoerder to notice changes

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar'
+import Message from './Components/Message'
+import Conversation from './Components/Conversation'
+
 
 const App = () => {
   return (
-    <div className='bg-red-500'>
-      Hello world
+    <div className='flex w-full'>
+     <Sidebar></Sidebar>
+     <Message></Message>
+     <Conversation></Conversation>
     </div>
   )
 }
