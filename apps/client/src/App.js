b6fce76b6f443a39bc08976d6125d928b2e99856
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-05 11:07:26 +0200

    disabled auto refactoring by prettier

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -3,7 +3,6 @@ import Sidebar from './Components/Sidebar'
 import Message from './Components/Message'
 import Conversation from './Components/Conversation'
 
-
 const App = () => {
   return (
     <div className='flex w-full'>
