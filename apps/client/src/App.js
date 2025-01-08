076d97ce40b03d689ca30f08ac2167b488642d7a
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-08 10:24:39 +0200

    imported all necessary imports

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -2,13 +2,17 @@ import React from 'react'
 import Sidebar from './Components/Sidebar'
 import Message from './Components/Message'
 import Conversation from './Components/Conversation'
+import Login from './Components/Login'
 
 const App = () => {
   return (
-    <div className='flex w-full'>
-     <Sidebar></Sidebar>
-     <Message></Message>
-     <Conversation></Conversation>
+    // <div className='flex w-full'>
+    //  <Sidebar></Sidebar>
+    //  <Message></Message>
+    //  <Conversation></Conversation>
+    // </div>
+    <div>
+      <Login></Login>
     </div>
   )
 }
