1e3380602cb188282fedc280328bdc503968c301
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-10 10:28:18 +0200

    Created boilercode for signup component

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -3,6 +3,7 @@ import Sidebar from './Components/Sidebar'
 import Message from './Components/Message'
 import Conversation from './Components/Conversation'
 import Login from './Components/Login'
+import Si
 
 const App = () => {
   return (
