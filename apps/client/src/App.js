af9e94dd9925f30a8156f6133cf61cde83d59a33
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-21 12:59:41 +0200

    Defined routes

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -4,6 +4,7 @@ import Message from './Components/Message'
 import Conversation from './Components/Conversation'
 import Login from './Components/Login'
 import Signup from './Components/Signup'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -12,10 +13,14 @@ const App = () => {
     //  <Message></Message>
     //  <Conversation></Conversation>
     // </div>
-    <div>
-      <Signup></Signup>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Login></Login>}></Route> */}
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/" element={<Signup></Signup>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
