3ea369cf0a58a85f5598900a94f0d049e2ed8588
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-24 09:38:26 +0200

    Imolemented logic for login handler

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -1,14 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateDto } from './dto/create.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('signup')
-    async signup(@Body() signupData: CreateDto){
-        console.log(signupData);
-        return this.authService.signup(signupData);
-    }
+  @Post('signup')
+  async signup(@Body() signupData: CreateDto) {
+    console.log(signupData);
+    return this.authService.signup(signupData);
+  }
+
+  @Post('login')
+  async login(@Body() loginData: LoginDto) {
+    return this.authService.login(loginData);
+  }
+
+  @Get('me')
+  me(){
+    return {mesage: "hello world"}
+  }
 }
