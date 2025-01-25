b6c8b52e61499bdbd16b37a191635c0350bd486d
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-25 09:41:42 +0200

    implemented login logic

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { CreateDto } from './dto/create.dto';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -16,5 +17,17 @@ export class AuthService {
 
     //return user
     return user;
+  }
+
+  async login(logindata: LoginDto) {
+    const user = await this.userservice.getUserByUsername(logindata.username);
+    if (user) {
+      const hash = await bcrypt.compare(logindata.password, user.password);
+      if (hash) {
+        return user;
+      }
+      throw new HttpException('incorrect credentials', HttpStatus.NOT_FOUND);
+    }
+    throw new HttpException('incorrect credentials', HttpStatus.NOT_FOUND);
   }
 }
