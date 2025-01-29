8e36f6a76a51d4aaad278ea4f12ab4eb291207a9
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-29 11:54:24 +0200

    Validated data

--- apps/server/src/auth/dto/create.dto.ts
+++ apps/server/src/auth/dto/create.dto.ts
@@ -1,7 +1,22 @@
+import { Exclude } from "class-transformer";
+import { IsEmail, isEmail, IsOptional, IsString } from "class-validator";
+
 export class CreateDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
   firstname: string;
+
+  @IsString()
+  @IsOptional()
   lastname: string;
+
+  @IsString()
+  @IsOptional()
   username: string;
+
+  @IsString()
+  @Exclude()
   password: string;
 }
