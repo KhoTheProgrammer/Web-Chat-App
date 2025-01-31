f62e2407b3c234d01dd880082d991afd47b048df
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-31 21:40:15 +0200

    Imported all necessary modules

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -4,9 +4,13 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, PassportModule, ConfigModule, JwtModule.register({
+    secret: process.env.SECRET_KEY,
+  })],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController]
 })
