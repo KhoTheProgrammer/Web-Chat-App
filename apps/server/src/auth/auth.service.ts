9f37257362098c8d674b812a155e2d20889db9a0
KhoTheProgrammer <kondwanipadyera@gmail.com>
2025-01-31 21:39:25 +0200

    Reimplemented login function to create and reuturn a token

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { UsersService } from 'src/users/users.service';
 import { CreateDto } from './dto/create.dto';
 import * as bcrypt from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userservice: UsersService) {}
+  constructor(
+    private userservice: UsersService,
+    private jwtservice: JwtService,
+    private config: ConfigService
+  ) {}
 
   async signup(signupdata: CreateDto) {
     //generate the password hash
@@ -19,16 +25,16 @@ export class AuthService {
     return user;
   }
 
-  async login(username: string, password: string) {
-    const user = await this.userservice.getUserByUsername(username);
-    if (user) {
-      const hash = await bcrypt.compare(password, user.password);
-      if (hash) {
-        return user;
-      }
-      throw new HttpException('incorrect credentials', HttpStatus.NOT_FOUND);
+  // Create token and return it to user
+  async login(user: any) {
+    try {
+      const payload = { username: user.username, sub: user.id };
+      return {
+        access_token: this.jwtservice.sign(payload),
+      };
+    } catch (error) {
+      
     }
-    throw new HttpException('incorrect credentials', HttpStatus.NOT_FOUND);
   }
 
   async validateUser(username: string, password: string) {
